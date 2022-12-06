ad45f6e792dceb550ed25fa0e89691d8f5aee51d
Marcos Venteo <mventeo@gmail.com>
2022-12-06 12:14:18 +0100

    Add SponsorsCursa

---
+++ src/components/SponsorsCursa.jsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import { Container } from './Container'
+
+export function SponsorsCursa() {
+  return (
+    <>
+      <div className="bg-white py-12 ">
+        <p className="text-center font-sans text-xl font-semibold uppercase text-red-400">
+          Patrocinador del Club
+        </p>
+        <div className="mx-auto max-w-7xl ">
+          <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-3">
+            <div className="col-span-3 flex justify-center">
+              <Link href="https://www.molletvalles.cat" target="_blank">
+                <picture>
+                  <source srcSet="/logos/mollet.jpg" media="image/png" />
+                  <img
+                    className="h-14 w-auto"
+                    src="/logos/mollet.jpg"
+                    alt="Tuple"
+                  />
+                </picture>
+              </Link>
+            </div>
+            <div className="col-span-3 flex justify-center">
+              <Link href="https://www.cubaspalau.com/" target="_blank">
+                <picture>
+                  <source srcSet="/logos/cubas-palau.png" media="image/png" />
+                  <img
+                    className="h-14 w-auto"
+                    src="/logos/cubas-palau.png"
+                    alt="Tuple"
+                  />
+                </picture>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
